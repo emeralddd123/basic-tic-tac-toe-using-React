9a6656cc1ebeca95f4d04243d0e641c159b47660
emeralddd123 <93847541+emeralddd123@users.noreply.github.com>
2023-01-25 19:55:26 +0100

    hands on react

---
+++ src/TicTacToe/TicTacToe.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import './TicTacToe.css'
+
+const TicTacToe = () => {
+  const { turn, setTurn } = useState('x')
+
+  const handleClick = (num) => {
+    if (turn === 'x') {
+      setTurn('o')
+    } else {
+      setTurn('x')
+    }
+  }
+
+  const Cell = ({ num }) => {
+    return <td onClick={() => handleClick(num)}>-</td>
+  }
+
+  return (
+    <div className='container'>
+      <table>
+        Turn: {turn}
+        <tbody>
+          <tr>
+            <Cell num={1} />
+            <Cell num={2} />
+            <Cell num={3} />
+          </tr>
+          <tr>
+            <Cell num={4} />
+            <Cell num={5} />
+            <Cell num={6} />
+          </tr>
+          <tr>
+            <Cell num={7} />
+            <Cell num={8} />
+            <Cell num={9} />
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default TicTacToe
